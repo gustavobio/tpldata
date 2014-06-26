a8a4e38fe61add4609f3e07e4dd2d5731f95839e
Gustavo Carvalho <gustavo.bio@gmail.com>
2014-06-25 23:26:21 -0300

    First commit

---
+++ R/help.R
@@ -0,0 +1,10 @@
+#' Package tpldata
+#' 
+#' The Plant List data
+#' 
+#' This package contains data from The Plant List database v1.1
+#' 
+#' @docType package
+#' @name tpldata
+#' @aliases tpldata tpldata-package
+NULL
