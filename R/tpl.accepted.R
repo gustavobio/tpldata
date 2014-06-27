6ee6c9805bbc4ebbb593d4d12099e43eea977caa
Gustavo Carvalho <gustavo.bio@gmail.com>
2014-06-27 01:12:58 -0300

    Bug fixes

---
+++ R/tpl.accepted.R
@@ -0,0 +1,9 @@
+#' The Plant List
+#'
+#' A few datasets containing data scraped from theplantlist.com
+#' 
+#' @docType data
+#' @format Lists and data frames
+#' @name tpl.accepted
+#' @aliases tpl.misapplied tpl.names tpl.synonyms, tpl.unresolved, words, tpl.accepted.index
+NULL
